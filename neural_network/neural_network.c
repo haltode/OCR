3e33c5402f3735151273ff4e88a9889c64e00d80
Thibault Allançon <tallancon@gmail.com>
2018-10-10 11:11:50 +0200

    neural_network: add core structure and alloc/free functions

--- neural_network/neural_network.c
+++ neural_network/neural_network.c
@@ -1,3 +1,43 @@
-void neural_network(void)
+#include <stdlib.h>
+
+#include "neural_network.h"
+
+static void layer_alloc(struct Layer *layer, size_t nb_in, size_t nb_out)
 {
+    layer->nb_neurons = nb_out;
+    layer->in = matrix_alloc(nb_in, 1);
+    layer->out = matrix_alloc(nb_out, 1);
+    layer->weight = matrix_alloc(nb_in, nb_out);
+    layer->bias = matrix_alloc(nb_out, 1);
+}
+
+static void layer_free(struct Layer *layer)
+{
+    matrix_free(layer->in);
+    matrix_free(layer->out);
+    matrix_free(layer->weight);
+    matrix_free(layer->bias);
+}
+
+struct Network *network_alloc(size_t nb_layers, size_t *layers_size)
+{
+    struct Network *network = malloc(sizeof(struct Network));
+    network->nb_layers = nb_layers;
+    network->layers = malloc(sizeof(struct Layer) * nb_layers);
+    for (size_t i = 0; i < nb_layers; i++)
+    {
+        size_t prev_layer_size = 0;
+        if (i > 0)
+            prev_layer_size = layers_size[i - 1];
+        layer_alloc(&network->layers[i], prev_layer_size, layers_size[i]);
+    }
+    return network;
+}
+
+void network_free(struct Network *network)
+{
+    for (size_t i = 0; i < network->nb_layers; i++)
+        layer_free(&network->layers[i]);
+    free(network->layers);
+    free(network);
 }
--- neural_network/neural_network.h
+++ neural_network/neural_network.h
@@ -1,4 +1,26 @@
 #ifndef NEURAL_NETWORK_H
 #define NEURAL_NETWORK_H
 
+#include <stddef.h>
+
+#include "../utils/matrix.h"
+
+struct Layer
+{
+    size_t nb_neurons;
+    struct Matrix *in;
+    struct Matrix *out;
+    struct Matrix *weight;
+    struct Matrix *bias;
+};
+
+struct Network
+{
+    size_t nb_layers;
+    struct Layer *layers;
+};
+
+struct Network *network_alloc(size_t nb_layers, size_t *layers_size);
+void network_free(struct Network *network);
+
 #endif

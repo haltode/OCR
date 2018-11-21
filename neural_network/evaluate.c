8062343dfe64e1665f07d1c139cbc29a71851289
Thibault Allançon <tallancon@gmail.com>
2018-11-21 22:10:34 +0100

    neural_network: generalize evaluation functions

--- neural_network/evaluate.c
+++ neural_network/evaluate.c
@@ -10,21 +10,34 @@ int network_run(struct Network *network, struct Matrix *network_input)
 
     struct Layer *output_layer = &network->layers[network->nb_layers - 1];
     float *output_vect = output_layer->out->mat;
-    size_t output_size = output_layer->out->nb_rows;
+    size_t output_size = matrix_size(output_layer->out);
 
     return argmax(output_vect, output_size);
 }
 
+bool is_network_correct(struct Network *network,
+    struct Matrix *network_input, struct Matrix *expected_output)
+{
+    int network_res = network_run(network, network_input);
+
+    float *output_vect = expected_output->mat;
+    size_t output_size = matrix_size(expected_output);
+    int expected_res = argmax(output_vect, output_size);
+
+    return (network_res == expected_res);
+}
+
 void network_evaluate(struct Network *network, struct TrainingSet *train_set)
 {
-    int nb_correct = 0;
+    size_t nb_correct = 0;
     for (size_t i = 0; i < train_set->params.nb_examples; i++)
     {
         struct TrainingData *example = &train_set->examples[i];
-        int res = network_run(network, example->in);
-        if (res == example->out->mat[0])
+        if (is_network_correct(network, example->in, example->out))
             nb_correct++;
     }
 
-    printf("accuracy: %d / %zu\n", nb_correct, train_set->params.nb_examples);
+    float percentage = 100 * nb_correct / train_set->params.nb_examples;
+    printf("accuracy: %zu / %zu (%.2f%%)\n",
+        nb_correct, train_set->params.nb_examples, percentage);
 }
--- neural_network/evaluate.h
+++ neural_network/evaluate.h
@@ -1,10 +1,14 @@
 #ifndef EVALUATE_H
 #define EVALUATE_H
 
+#include <stdbool.h>
+
 #include "neural_network.h"
 #include "training/training.h"
 
 int network_run(struct Network *network, struct Matrix *network_input);
+bool is_network_correct(struct Network *network,
+    struct Matrix *network_input, struct Matrix *expected_output);
 void network_evaluate(struct Network *network, struct TrainingSet *train_set);
 
 #endif

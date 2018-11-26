3a83b1426f340e405e9356bf7fec3e911349f084
Thibault Allançon <tallancon@gmail.com>
2018-11-26 22:01:57 +0100

    dataset: increase chars repetition (2000 -> 2500)

--- dataset/generate_scrambled.py
+++ dataset/generate_scrambled.py
@@ -6,7 +6,7 @@ charset = ("abcdefghijklmnopqrstuvwxyz"
            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
            "0123456789"
            ".,")
-repeat_char = 2000
+repeat_char = 2500
 
 out = []
 for c in charset:
@@ -19,15 +19,18 @@ random.shuffle(out)
 cnt = 0
 with open('scrambled.txt', 'w') as txt, open('scrambled.tex', 'w') as tex:
     for c in out:
-        txt.write(c), tex.write(c)
+        txt.write(c)
+        tex.write(c)
         cnt += 1
         # Indent (80 chars / line)
         if cnt % 80 == 0:
-            txt.write('\n'), tex.write('\n')
-        # Page break (4000 chars / page)
-        if cnt % 4000 == 0:
+            txt.write('\n')
+            tex.write('\\\\\n')
+        # Page break (5000 chars / page)
+        if cnt % 5000 == 0:
             cnt = 0
-            txt.write('\n\n'), tex.write('\n\\newpage\n')
+            txt.write('\n\n')
+            tex.write('\n\\newpage\n')
 
 print("charset size:", len(charset))
 print("total chars generated:", repeat_char * len(charset))
